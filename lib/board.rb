e4c4952deced70b0ea2324f4693e1c47a9a54a38
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2024-10-13 15:15:09 +0200

    write tests for get_move_index method

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,30 @@
 class Board 
+  attr_accessor :board_array
+
   def initialize
+    @board_array = [
+      "⚫️",  "⚫️",  "⚫️",  "⚫️",  "⚫️",  "⚫️",  "⚫️",
+      "⚫️",  "⚫️",  "⚫️",  "⚫️",  "⚫️",  "⚫️",  "⚫️",
+      "⚫️",  "⚫️",  "⚫️",  "⚫️",  "⚫️",  "⚫️",  "⚫️",
+      "⚫️",  "⚫️",  "⚫️",  "⚫️",  "⚫️",  "⚫️",  "⚫️",
+      "⚫️",  "⚫️",  "⚫️",  "⚫️",  "⚫️",  "⚫️",  "⚫️",
+      "⚫️",  "⚫️",  "⚫️",  "⚫️",  "⚫️",  "⚫️",  "⚫️"
+    ]
   end
+
+  def get_move_index(column)
+    current_row = 0
+
+    while @board_array[current_row*7 + column] == "⚫️"
+      current_row += 1
+    end
+
+    (current_row - 1)*7 + column
+  end
+
+  # make move should
+  # - get the column
+  # - get the precise baord index corresponding to the first free spot on the column starting from the bottom
+  # which translates to checking the last free spot of the column starting from the top
+  # should call a method to get that index
 end
